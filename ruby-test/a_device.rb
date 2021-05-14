8be809b921d1a764cd9656045e5e9d7542ce955a
Oscar Cortes <olcortesb@gmail.com>
2021-05-14 16:23:38 -0300

    fix:change argv defaul

--- ruby-test/a_device.rb
+++ ruby-test/a_device.rb
@@ -8,12 +8,15 @@ puts ARGV[0]
 API_1 = 'https://kgczmkfv0c.execute-api.us-west-2.amazonaws.com/stage/data'
 API_2 = 'https://xqx400kz78.execute-api.us-west-2.amazonaws.com/stage/edge'
 
-API =  ARGV[1] == 1 ? API_1 : API_2
+puts ARGV[1]
+
+API = ARGV[1].to_i == 1 ? API_1 : API_2
 
 
 payload = { device: 1, status: 1, message: ARGV[0] }
 response = RestClient.post API, payload.to_json, {content_type: :json, accept: :json}
 
+puts API
 puts response.code 
 puts response.body
 
